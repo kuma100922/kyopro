e250026dd0f9761c4c9d5ab5e95b9b4d585325b3
kuma100922 <50899789+kuma100922@users.noreply.github.com>
2026-05-02 06:26:08 +0900

    途中まででコミット
    また逢う日まで!

---
+++ code/1/D/code.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int r;
+    cin >> r;
+
+    vector<int><int> mass();
+
+    return 0;
+}
---
+++ reference/template.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int q;
+    cin >> q;
+
+    vector<int> snakes(q);
+
+    return 0;
+}
